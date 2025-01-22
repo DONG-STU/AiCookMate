e845249d7b6ba655ff648327007092c14acc3c4d
DreamB <luisluos@gmail.com>
2025-01-22 11:45:50 +0900

    Design BottomBar

--- app/src/main/java/com/sdc/aicookmate/MainHomeActivity.kt
+++ app/src/main/java/com/sdc/aicookmate/MainHomeActivity.kt
@@ -1,8 +1,6 @@
 package com.sdc.aicookmate
 
 
-import FoodCategoriesScreen.ZzimRecipeList
-import FoodCategoriesScreen.ZzimViewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -10,9 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -28,16 +25,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -46,16 +38,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.sdc.aicookmate.ui.theme.AiCookMateTheme
 
 @Composable
 fun MainScreen(navController: NavController) {
@@ -473,51 +461,53 @@ fun BottomBar(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(56.dp)
-            .background(Color(0xFFF2F2F2))
+            .wrapContentHeight()
+            .padding(5.dp)
+            .background(Color.White)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Spacer(modifier = Modifier.weight(1f))
             Image(
-                painter = painterResource(id = R.drawable.ic_home),
+                painter = painterResource(R.drawable.home_btn),
                 contentDescription = "메인 홈 아이콘",
                 modifier = Modifier
                     .size(40.dp)
+                    .weight(1f)
                     .clickable { navController.navigate("main") }
             )
+            Spacer(modifier = Modifier.weight(1f))
             Image(
-                painter = painterResource(id = R.drawable.ic_cart),
-                contentDescription = "카트 아이콘",
+                painter = painterResource(R.drawable.shopping_btn),
+                contentDescription = "쇼핑 아이콘",
                 modifier = Modifier
                     .size(40.dp)
+                    .weight(1f)
                     .clickable { navController.navigate("shopping") }
             )
+            Spacer(modifier = Modifier.weight(1f))
             Image(
-                painter = painterResource(id = R.drawable.ic_kitchen),
-                contentDescription = "냉장고 아이콘",
+                painter = painterResource(R.drawable.recipe_book),
+                contentDescription = "레시피 아이콘",
                 modifier = Modifier
                     .size(40.dp)
-                    .clickable {navController.navigate("refigeratorScreen") }
-            )
-            Image(
-                painter = painterResource(id = R.drawable.ic_bookheart),
-                contentDescription = "찜 레시피 아이콘",
-                modifier = Modifier
-                    .size(40.dp)
+                    .weight(1f)
                     .clickable { navController.navigate("Recipe") }
             )
+            Spacer(modifier = Modifier.weight(1f))
             Image(
-                painter = painterResource(id = R.drawable.ic_person),
+                painter = painterResource(R.drawable.mypage_btn),
                 contentDescription = "마이페이지 아이콘",
                 modifier = Modifier
                     .size(40.dp)
+                    .weight(1f)
                     .clickable { navController.navigate("myPage") }
             )
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
