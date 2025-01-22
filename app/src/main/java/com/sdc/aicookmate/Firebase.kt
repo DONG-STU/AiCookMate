678f757e3aca2f7943f9f9e21886b1dbd4deea35
dong chan <sts05147@naver.com>
2025-01-22 11:59:52 +0900

    firebase image edit complete

--- app/src/main/java/com/sdc/aicookmate/Firebase.kt
+++ app/src/main/java/com/sdc/aicookmate/Firebase.kt
@@ -3,6 +3,7 @@ package com.sdc.aicookmate
 
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -22,8 +23,10 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import coil3.compose.rememberAsyncImagePainter
 import com.google.firebase.firestore.FieldPath
 import kotlinx.coroutines.Job
@@ -74,6 +77,7 @@ class RecipeViewModel : ViewModel() {
                 }
         }
     }
+
     // 드롭다운용 검색 쿼리
     fun fetchRecipes2(query: String) {
         searchJob?.cancel()
@@ -125,32 +129,44 @@ fun RecipeItem(item: RecipeData, onClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
+            .padding(vertical = 8.dp)
+            .background(Color.White),
         shape = RoundedCornerShape(12.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Image(
-                painter = rememberAsyncImagePainter(item.thumbnail),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .clickable {
-                        onClick(Uri.encode(item.title))
-                    }
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(text = item.title, modifier = Modifier.padding(bottom = 12.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Image(
+                    painter = rememberAsyncImagePainter(item.thumbnail),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clickable {
+                            onClick(Uri.encode(item.title))
+                        }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = item.title,
+                    modifier = Modifier.padding(bottom = 12.dp),
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(60.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                InfoRow(iconRes = R.drawable.ic_person, text = item.servings)
-                InfoRow(iconRes = R.drawable.ic_time, text = item.time_required)
-                InfoRow(iconRes = R.drawable.ic_star, text = item.difficulty)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(60.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    InfoRow(iconRes = R.drawable.ic_person, text = item.servings)
+                    InfoRow(iconRes = R.drawable.ic_time, text = item.time_required)
+                    InfoRow(iconRes = R.drawable.ic_star, text = item.difficulty)
+                }
             }
         }
     }
@@ -168,7 +184,7 @@ fun InfoRow(iconRes: Int, text: String) {
             modifier = Modifier.size(16.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Text(text = text)
+        Text(text = text, fontWeight = FontWeight.Bold, color = Color.Black)
     }
 }
 
